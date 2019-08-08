b4127be2f84f5fc62d1c72c2c351825b140e68b1
Mo Ramezanpoor <me@mohsenr.com>
2019-08-08 16:52:29 +0100

    Add documentation for `XCTAssertFatalError`.

--- Sources/TestingSupport/XCTAssertFatalError.swift
+++ Sources/TestingSupport/XCTAssertFatalError.swift
@@ -1,6 +1,9 @@
 import XCTest
 import Support
 
+/// Asserts that the provided closure calls `Thread.fatalError`.
+///
+/// `work` is invoked on a separate thread. Do not rely on thread-local information from the current thread in this closure.
 public func XCTAssertFatalError(_ message: String = "Expected fatal error", file: StaticString = #file, line: UInt = #line, _ work: @escaping () -> Void) {
     let manner = Thread.detachSyncSupervised(work)
     
