c3c8b927fec9911da54f7034717c15c6127005a9
Mo Ramezanpoor <me@mohsenr.com>
2019-11-01 15:45:49 +0100

    Set min macOS version to 10.15.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Support",
     platforms: [
-        .macOS(.v10_14),
+        .macOS(.v10_15),
         .iOS(.v13),
         .tvOS(.v13),
         .watchOS(.v6),
