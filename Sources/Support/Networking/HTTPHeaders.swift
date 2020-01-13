dfa62be65c03c979c6f2aae9ed95a309fadc0459
Mo Ramezanpoor <me@mohsenr.com>
2020-01-13 16:28:36 +0000

    Remove dynamic member lookup conformance on `HTTPHeaders`.

--- Sources/Support/Networking/HTTPHeaders.swift
+++ Sources/Support/Networking/HTTPHeaders.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-@dynamicMemberLookup
 public struct HTTPHeaders: ExpressibleByDictionaryLiteral, Equatable {
     
     public var fields: [HTTPHeaderFieldName: String]
@@ -18,15 +17,6 @@ public struct HTTPHeaders: ExpressibleByDictionaryLiteral, Equatable {
             Thread.fatalError("Duplicate header fields: \(fields).")
         }
         self.init(fields: fields)
-    }
-    
-    public subscript<Value>(dynamicMember keyPath: WritableKeyPath<[HTTPHeaderFieldName: String], Value>) -> Value {
-        get {
-            fields[keyPath: keyPath]
-        }
-        set {
-            fields[keyPath: keyPath] = newValue
-        }
     }
     
     public func hasValue(for name: HTTPHeaderFieldName) -> Bool {
