f0a37928fe7a919ffb5e9f8527a0a056d84c84d4
Mo Ramezanpoor <me@mohsenr.com>
2020-01-31 13:08:50 +0000

    Remove a deprecated method.

--- Sources/TestingSupport/Assertions/TS+assertFatalError.swift
+++ Sources/TestingSupport/Assertions/TS+assertFatalError.swift
@@ -1,14 +1,6 @@
 import Support
 import XCTest
 
-/// Asserts that the provided closure calls `Thread.fatalError`.
-///
-/// `work` is invoked on a separate thread. Do not rely on thread-local information from the current thread in this closure.
-@available(*, deprecated, renamed: "TS.assertFatalError")
-public func XCTAssertFatalError(_ message: String = "Expected fatal error", file: StaticString = #file, line: UInt = #line, _ work: @escaping () -> Void) {
-    TS.assertFatalError(message, file: file, line: line, work)
-}
-
 extension TS {
     
     public static func assertFatalError(_ message: String = "Expected fatal error", file: StaticString = #file, line: UInt = #line, _ work: @escaping () -> Void) {
