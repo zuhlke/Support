4537c2b7019d3e53f0d927449bb97dda7bcf7931
Mo Ramezanpoor <me@mohsenr.com>
2020-03-08 14:06:40 +0000

    Make `PublisherEventKind` equatable and provide a description for it.

--- Sources/Support/Combine/PublisherEventKind.swift
+++ Sources/Support/Combine/PublisherEventKind.swift
@@ -1,11 +1,21 @@
 import Combine
 import Foundation
 
-public class PublisherEventKind {
+public class PublisherEventKind: Equatable, CustomStringConvertible {
+    private let label: String
     fileprivate let regulator: PublisherRegulator
     
-    public init(regulator: PublisherRegulator) {
+    public init(label: String = "\(#file).\(#function)L\(#line)", regulator: PublisherRegulator) {
+        self.label = label
         self.regulator = regulator
+    }
+    
+    public static func == (lhs: PublisherEventKind, rhs: PublisherEventKind) -> Bool {
+        lhs === rhs
+    }
+    
+    public var description: String {
+        "\(Self.self)(\(label))"
     }
 }
 
