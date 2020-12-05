395e9d594981a6d97c48f7f4df3aa8cf3b1cd823
Mo Ramezanpoor <me@mohsenr.com>
2020-12-05 11:28:33 +0000

    Document `URLScheme` API.

--- Sources/Support/Networking/URLScheme.swift
+++ Sources/Support/Networking/URLScheme.swift
@@ -1,9 +1,14 @@
 import Foundation
 
+/// A URL scheme.
+///
+/// Use this in conjunction with the networking API.
 public struct URLScheme: Equatable {
     
     var normalizedValue: String
     
+    /// Creates a URL scheme with the `rawValue`
+    /// - Parameter rawValue: Raw value of the scheme. This is case insensitive.
     public init(_ rawValue: String) {
         normalizedValue = rawValue.lowercased()
     }
@@ -12,8 +17,10 @@ public struct URLScheme: Equatable {
 
 extension URLScheme {
     
+    /// HTTPS URL scheme
     public static let https = URLScheme("https")
     
+    /// Secure WebSockets URL scheme
     public static let wss = URLScheme("wss")
     
 }
