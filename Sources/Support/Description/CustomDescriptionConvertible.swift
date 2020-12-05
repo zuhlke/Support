d5cb798e47fbeff26a62909f57a8868dd85cd8b4
Mo Ramezanpoor <me@mohsenr.com>
2020-12-05 11:11:48 +0000

    Move Data’s conformance of `CustomDescriptionConvertible` to Support.

--- Sources/Support/Description/CustomDescriptionConvertible.swift
+++ Sources/Support/Description/CustomDescriptionConvertible.swift
@@ -3,3 +3,15 @@ import Foundation
 public protocol CustomDescriptionConvertible {
     var structuredDescription: Description { get }
 }
+
+extension Data: CustomDescriptionConvertible {
+    public var structuredDescription: Description {
+        if let string = String(data: self, encoding: .utf8) {
+            return .string(string)
+        } else {
+            return .dictionary([
+                "base64Encoded": .string(base64EncodedString())
+            ])
+        }
+    }
+}
--- Sources/TestingSupport/Extensions/Data.swift
+++ Sources/TestingSupport/Extensions/Data.swift
@@ -12,13 +12,3 @@ extension Data {
     }
     
 }
-
-extension Data: CustomDescriptionConvertible {
-    public var structuredDescription: Description {
-        if let string = String(data: self, encoding: .utf8) {
-            return .string(string)
-        } else {
-            return .string(base64EncodedString())
-        }
-    }
-}
