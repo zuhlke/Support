a254b31f993464cff3e196362e4af51209e28f59
Mo Ramezanpoor <me@mohsenr.com>
2020-12-05 12:45:04 +0000

    Provide a way of synthesising a whole generated object.

---
+++ Sources/TestingSupport/PropertyTesting/AutoGeneratable.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public protocol AutoGeneratable: RandomCasesGeneratable {
+    init()
+}
+
+extension AutoGeneratable where Configuration == EmptyConfiguration {
+    public static func makeRandomCasesGenerator<RNG>(with configuration: Configuration, numberGenerator: RNG) -> AnySamplingGenerator<Self> where RNG : RandomNumberGenerator {
+        AnySamplingGenerator(state: numberGenerator) { numberGenerator in
+            self.init(with: &numberGenerator)
+        }
+    }
+}
+
+extension AutoGeneratable {
+    init<RNG: RandomNumberGenerator>(with numberGenerator: inout RNG) {
+        self.init()
+        randomize(with: &numberGenerator)
+    }
+    
+    func randomize<RNG: RandomNumberGenerator>(with numberGenerator: inout RNG) {
+        Mirror(reflecting: self).children.forEach { _, child in
+            if let randomizable = child as? Randomizable {
+                randomizable.randomize(with: &numberGenerator)
+            }
+        }
+    }
+}
