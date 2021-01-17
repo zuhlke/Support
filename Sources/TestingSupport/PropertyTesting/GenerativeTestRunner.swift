cb8bedd08ad76f6d3543227645425f09c99a878e
Mo Ramezanpoor <me@mohsenr.com>
2021-01-17 12:41:18 +0000

    Support shrinking.

--- Sources/TestingSupport/PropertyTesting/GenerativeTestRunner.swift
+++ Sources/TestingSupport/PropertyTesting/GenerativeTestRunner.swift
@@ -23,8 +23,24 @@ public struct GenerativeTestRunner {
         Gen.Element == Seq.Element
     {
         try elements.prefix(maxIterations).forEach { element in
-            try action(element)
+            do {
+                try action(element)
+            } catch {
+                throw shrink(failingElement: element, generator: generator, previousError: error, action: action)
+            }
         }
+    }
+    
+    private func shrink<Gen>(failingElement: Gen.Element, generator: Gen, previousError: Error, action: (Gen.Element) throws -> Void) -> Error where Gen: SamplingGenerator {
+        let shrunkGenerator = generator.shrink(failingElement)
+        for element in shrunkGenerator.allElements {
+            do {
+                try action(element)
+            } catch {
+                return shrink(failingElement: element, generator: shrunkGenerator, previousError: error, action: action)
+            }
+        }
+        return previousError
     }
     
 }
