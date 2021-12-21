40ae4af5ec90108eb321da898db588c4c3b13d7c
Mo Ramezanpoor <me@mohsenr.com>
2021-12-21 14:22:58 +0000

    Create public init for `ProjectFile`.

--- Sources/InfraSupport/Data/ProjectFile.swift
+++ Sources/InfraSupport/Data/ProjectFile.swift
@@ -2,9 +2,15 @@ import Foundation
 
 /// Represents a file in the repository.
 public struct ProjectFile {
+    
     /// Path of the file, relative to the repository’s root folder.
     public var pathInRepository: String
     
     /// Contents of the file.
     public var contents: String
+    
+    public init(pathInRepository: String, contents: String) {
+        self.pathInRepository = pathInRepository
+        self.contents = contents
+    }
 }
