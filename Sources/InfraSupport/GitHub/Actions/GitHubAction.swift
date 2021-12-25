1516635fb87d22ff056a516412bfec5beb0f8830
Mo Ramezanpoor <me@mohsenr.com>
2021-12-25 16:41:47 +0000

    Define `GitHubAction` protocol.

---
+++ Sources/InfraSupport/GitHub/Actions/GitHubAction.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+/// A type representing a GitHub action.
+///
+/// The action has an associate type, `Inputs`. This type should conform to `ParameterSet` protocol,
+/// and should only consist of properties wrapped by `ActionInput` type.
+/// These property wrappers must be visited when the parameter set is being encoded.
+public protocol GitHubAction {
+    #warning("Provide an example `Inputs` conformance as part of docs after refining the types.")
+    // Upload artefact action is a good example: https://github.com/actions/upload-artifact/blob/main/action.yml
+    typealias ParameterSet = GitHubActionParameterSet
+    associatedtype Inputs: ParameterSet = EmptyGitHubLocalActionParameterSet
+    
+    var name: String { get }
+}
