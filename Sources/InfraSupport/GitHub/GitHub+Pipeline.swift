0b4c18c3877d6bd240eb61282624c8c477c704af
Mo Ramezanpoor <me@mohsenr.com>
2021-12-25 16:21:12 +0000

    Provide an init of `GitHub.Pipeline` which can extract actions from a workflow file.

--- Sources/InfraSupport/GitHub/GitHub+Pipeline.swift
+++ Sources/InfraSupport/GitHub/GitHub+Pipeline.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Support
+import YAMLBuilder
 
 extension GitHub {
     
@@ -7,10 +8,35 @@ extension GitHub {
     public struct Pipeline {
         var actions: [Action]
         var workflows: [Workflow]
+    }
+}
+
+extension GitHub.Pipeline {
+    
+    /// Creates a github pipeline.
+    ///
+    /// The resulting pipeline will contain the defition for the `workflows`,
+    /// in addition to any local actions references as part of these worflows.
+    ///
+    /// The initialiser performs some validation on the workflow files, and may throw errors if it finds issues.
+    /// The exact validations performed may evolve in each version of the package.
+    /// Therefore, it’s highly recommended to re-run this pipeline after a version upgrade to ensure the definitions are still valid.
+    public init(@WorkflowsBuilder workflows: () -> [GitHub.Workflow]) throws {
+        let workflows = workflows()
+        let actions = workflows.lazy
+            .flatMap(\.jobs)
+            .flatMap(\.steps)
+            .compactMap(\.actionDefinition)
         
-        public init(actions: [GitHub.Action], workflows: [GitHub.Workflow]) {
-            self.actions = actions
-            self.workflows = workflows
-        }
+        self.init(actions: Array(actions), workflows: workflows)
+    }
+    
+}
+
+@resultBuilder
+public class WorkflowsBuilder: ArrayBuilder<GitHub.Workflow> {
+    
+    public static func buildFinalResult(_ workflows: [GitHub.Workflow]) -> [GitHub.Workflow] {
+        workflows
     }
 }
---
+++ Tests/InfraSupportTests/GitHubPipelineTests.swift
@@ -0,0 +1,38 @@
+import Support
+import TestingSupport
+import XCTest
+@testable import InfraSupport
+
+class GitHubPipelineTests: XCTestCase {
+    
+    func testCreatingPipelineExtractsActionsFromWorkflow() throws {
+        let action = MockActionWithoutInputs()
+        let workflow = GitHub.Workflow(id: .random(), name: .random()) {
+            .init()
+        } jobs: {
+            Job(id: .random(), name: .random(), runsOn: .macos11) {
+                Job.Step(action: action)
+            }
+        }
+        let actual = try GitHub.Pipeline {
+            workflow
+        }
+        let expected = GitHub.Pipeline(
+            actions: [.init(action)],
+            workflows: [workflow]
+        )
+        let encoder = GitHub.MetadataEncoder()
+        TS.assert(encoder.projectFiles(for: actual), equals: encoder.projectFiles(for: expected))
+    }
+    
+}
+
+private struct MockActionWithoutInputs: GitHubLocalAction {
+    var id = "local-action-id"
+    var name = "Local Action Name"
+    var description = String.random()
+    
+    func run(inputs: InputAccessor<Inputs>, outputs: OutputAccessor<Outputs>) -> GitHub.Action.Run {
+        Composite {}
+    }
+}
