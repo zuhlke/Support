5d7868a67c358249774890445f52fd850dbaa0c4
Mo Ramezanpoor <me@mohsenr.com>
2022-04-09 14:43:06 +0100

    Document `HTTPRequest.Body`.

--- Sources/Support/Networking/HTTPRequest+Body.swift
+++ Sources/Support/Networking/HTTPRequest+Body.swift
@@ -1,6 +1,9 @@
 import Foundation
 
 extension HTTPRequest {
+    /// An HTTP request’s payload.
+    ///
+    /// In addition to payload’s data, this type specifies the content-type.
     public struct Body: Equatable {
         public let content: Data
         public let type: String
@@ -13,6 +16,7 @@ extension HTTPRequest {
 
 extension HTTPRequest.Body {
     
+    /// Creates a body with the specified data and `text/plain` content type.
     public static func plain(_ data: Data) -> HTTPRequest.Body {
         HTTPRequest.Body(
             content: data,
@@ -20,15 +24,22 @@ extension HTTPRequest.Body {
         )
     }
     
+    /// Creates a body with the specified text encoded as utf8 and `text/plain` content type.
     public static func plain(_ text: String) -> HTTPRequest.Body {
-        .plain(text.data(using: .utf8)!)
+        .plain(Data(text.utf8))
     }
     
+    /// Creates a body with the specified data and `application/json` content type.
     public static func json(_ data: Data) -> HTTPRequest.Body {
         HTTPRequest.Body(
             content: data,
             type: "application/json"
         )
+    }
+    
+    /// Creates a body by encoding `content` and using `application/json` content type.
+    public static func json<Content: Encodable>(_ content: Content, encoder: JSONEncoder = JSONEncoder()) throws -> HTTPRequest.Body {
+        return .json(try encoder.encode(content))
     }
     
 }
