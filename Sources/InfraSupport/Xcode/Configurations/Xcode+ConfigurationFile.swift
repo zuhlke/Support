e4792672154356d9dc85d7dc38f8e32db49867ac
Mo Ramezanpoor <me@mohsenr.com>
2022-08-21 14:54:41 +0100

    `Xcode.ConfigurationFile` can validate that the contents it is passed is well-formed.

--- Sources/InfraSupport/Xcode/Configurations/Xcode+ConfigurationFile.swift
+++ Sources/InfraSupport/Xcode/Configurations/Xcode+ConfigurationFile.swift
@@ -5,10 +5,88 @@ extension Xcode {
     /// Represents an Xcode configuration (`xcconfig`) file.
     public struct ConfigurationFile {
         
+        public struct Line {
+            var content: Substring
+        }
+        
+        private struct MalformedConfiguration: Error {
+            var contents: String
+        }
+        
+        private var lines: [Line]
+        
+        /// A configuration file.
+        /// - Parameter url: URL of the file to load.
+        /// - throws: An error if it can not load the file, or if it doesn’t contain a well-formed configuration file.
         public init(contentsOf url: URL) throws {
-            _ = try String(contentsOf: url)
+            let contents = try String(contentsOf: url)
+            let scanner = Scanner(string: contents)
+            scanner.charactersToBeSkipped = .whitespaces
+            lines = []
+            while !scanner.isAtEnd {
+                let startIndex = scanner.currentIndex
+                if scanner.scanString("#include ") != nil { // Include
+                    guard
+                        let quote = scanner.scanCharacter(),
+                        (quote == "\"" || quote == "'"),
+                        scanner.scanUpToString(String(quote)) != nil,
+                        scanner.scanString(String(quote)) != nil
+                    else {
+                        throw MalformedConfiguration(contents: contents)
+                    }
+                } else if scanner.scanCharacters(from: .variableStart) != nil { // Assignment
+                    scanner.charactersToBeSkipped = nil
+                    _ = scanner.scanCharacters(from: .variable)
+                    scanner.charactersToBeSkipped = .whitespaces
+
+                    while scanner.scanString("[") != nil {
+                        repeat {
+                            let keywords = ["sdk", "arch", "config"]
+                            let keyword = keywords.first { scanner.scanString($0) != nil }
+                            guard keyword != nil, scanner.scanString("=") != nil, scanner.scanCharacters(from: .conditionValue) != nil else {
+                                throw MalformedConfiguration(contents: contents)
+                            }
+                        } while scanner.scanString(",") != nil
+
+                        guard scanner.scanString("]") != nil else {
+                            throw MalformedConfiguration(contents: contents)
+                        }
+                    }
+                    
+                    guard scanner.scanString("=") != nil else {
+                        throw MalformedConfiguration(contents: contents)
+                    }
+                    _ = scanner.scanUpToString("\n")
+                }
+                
+                _ = scanner.scanString(";")
+                
+                if scanner.scanString("//") != nil { // Comment
+                    _ = scanner.scanUpToString("\n")
+                }
+                
+                guard scanner.isAtEnd || scanner.scanString("\n") != nil else {
+                    throw MalformedConfiguration(contents: contents)
+                }
+                let endIndex = scanner.currentIndex
+                lines.append(Line(content: contents[startIndex..<endIndex]))
+            }
         }
         
     }
+    
+}
+
+private extension CharacterSet {
+    
+    static let variableStart = CharacterSet(charactersIn: "_")
+        .union(CharacterSet(charactersIn: "a" ... "z"))
+        .union(CharacterSet(charactersIn: "A" ... "Z"))
+    
+    static let variable = CharacterSet.variableStart
+        .union(CharacterSet(charactersIn: "0" ... "9"))
+    
+    static let conditionValue = CharacterSet.variable
+        .union(CharacterSet(charactersIn: "*"))
     
 }
--- Tests/InfraSupportTests/Xcode/XcodeConfigurationFileTests.swift
+++ Tests/InfraSupportTests/Xcode/XcodeConfigurationFileTests.swift
@@ -12,11 +12,76 @@ final class XcodeConfigurationFileTests: XCTestCase {
         })
     }
     
-    func testLoadingEmptyFilePasses() throws {
+    func testLoadingEmptyFile() throws {
+        _ = try load("")
+    }
+    
+    func testLoadingARandomInvalidFile() throws {
+        // Any content that has alphanumberic value bus isn’t an assignment, comment, or import is definitely malformed.
+        let contents = String.random()
+        
+        // Just making sure our assumptions about implementation of random holds.
+        XCTAssertNil(contents.rangeOfCharacter(from: CharacterSet(charactersIn: "=/#")))
+        XCTAssertNotNil(contents.rangeOfCharacter(from: .alphanumerics))
+        XCTAssertThrowsError(try load(contents))
+    }
+    
+    func testLoadingComments() throws {
+        _ = try load("//")
+        _ = try load(" // content comes here")
+        _ = try load("  //  ")
+        XCTAssertThrowsError(try load("not a comment // "))
+    }
+    
+    func testLoadingImports() throws {
+        _ = try load(#"#include "a.xcconfig""#)
+        _ = try load(#"#include "../some/path.xcconfig""#)
+        _ = try load(#"#include     "a.xcconfig""#)
+        _ = try load(#"#include 'a.xcconfig'"#)
+        _ = try load(#" #include 'a.xcconfig'"#)
+        _ = try load(#" #include 'a.xcconfig';"#)
+        _ = try load(#" #include 'a.xcconfig' // comment"#)
+        XCTAssertThrowsError(try load(#"#include"#)) // no include
+        XCTAssertThrowsError(try load(#"#include"a.xcconfig""#)) // no whitespace after `include`
+        XCTAssertThrowsError(try load(#"#include "a.xcconfig"#)) // missing end quote
+        XCTAssertThrowsError(try load(#"#include a.xcconfig""#)) // missing start quote
+        XCTAssertThrowsError(try load(#"#include """#)) // empty include
+        XCTAssertThrowsError(try load(#"#include "a.xcconfig"""#)) // extra end quote
+    }
+    
+    func testLoadingAssignments() throws {
+        _ = try load(#"a=b"#)
+        _ = try load(#"a= b"#)
+        _ = try load(#"a =b"#)
+        _ = try load(#"a ="#)
+        _ = try load(#"a = b"#)
+        _ = try load(#"a = b;"#)
+        _ = try load(#"_underscored ="#)
+        _ = try load(#"lower ="#)
+        _ = try load(#"Upper ="#)
+        for key in ["sdk", "arch", "config"] {
+            _ = try load(#"conditional[\#(key)=value] = b"#)
+            _ = try load(#"conditional[\#(key)=value*] = b"#)
+            _ = try load(#"conditional[\#(key)=*] = b"#)
+        }
+        _ = try load(#"conditional[sdk=*][arch=*] = b"#)
+        _ = try load(#"conditional[arch=*][sdk=*] = b"#)
+        _ = try load(#"conditional[sdk=*][arch=*][config=*] = b"#)
+        _ = try load(#"conditional[sdk=*,arch=*] = b"#)
+        _ = try load(#"conditional[arch=*,sdk=*] = b"#)
+        _ = try load(#"conditional[sdk=*,arch=*,config=*] = b"#)
+        XCTAssertThrowsError(try load(#" ="#)) // No variable name
+        XCTAssertThrowsError(try load(#" =b"#)) // No variable name
+        XCTAssertThrowsError(try load(#"0variable =b"#)) // Variable name starting with number
+        XCTAssertThrowsError(try load(#"variable name =b"#)) // Variable name with a space
+    }
+
+    private func load(_ contents: String) throws -> Xcode.ConfigurationFile {
         try FileManager().makeTemporaryDirectory(perform: { url in
             let file = url.appendingPathComponent(.random())
-            try Data().write(to: file)
-            _ = try Xcode.ConfigurationFile(contentsOf: file)
+            
+            try contents.write(to: file, atomically: true, encoding: .utf8)
+            return try .init(contentsOf: file)
         })
     }
     
