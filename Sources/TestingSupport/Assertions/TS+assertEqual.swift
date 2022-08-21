cb9bc06b9deeb5776cc735a04b0f45866013437b
Mo Ramezanpoor <me@mohsenr.com>
2022-08-21 15:10:58 +0100

    Refine `TS.assert(:equal:)` to improve overload detection.

--- Sources/TestingSupport/Assertions/TS+assertEqual.swift
+++ Sources/TestingSupport/Assertions/TS+assertEqual.swift
@@ -3,6 +3,10 @@ import Support
 
 extension TS {
     
+    /// Asserts that `actual` and `expected` are equal.
+    ///
+    /// Functionally, this is equivalent to `XCTAssertEqual`. However, if the values are not equal, this method provides better diagnostics by generating a diff
+    /// between the `actual` and `expected` values.
     public static func assert<T>(
         _ actual: @autoclosure () throws -> T,
         equals expected: @autoclosure () throws -> T,
@@ -32,7 +36,7 @@ extension TS {
         let normalize = { (value: T) -> T in
             normalizations.reduce(value) { $1.normalize($0) }
         }
-        assert(
+        assertNormalized(
             normalize(try actual()),
             equals: normalize(try expected()),
             message(),
@@ -45,7 +49,7 @@ extension TS {
     ///
     /// Functionally, this is equivalent to `XCTAssertEqual`. However, if the values are not equal, this method provides better diagnostics by generating a diff
     /// between the `actual` and `expected` values.
-    public static func assert<T>(
+    private static func assertNormalized<T>(
         _ actual: @autoclosure () throws -> T,
         equals expected: @autoclosure () throws -> T,
         _ message: @autoclosure () -> String = "",
@@ -56,7 +60,7 @@ extension TS {
             let actualValue = try actual()
             let expectedValue = try expected()
             guard actualValue != expectedValue else { return }
-            
+
             let message = differenceMessage(expected: expectedValue, actual: actualValue)
             fail(message, file: file, line: line)
         } catch {
