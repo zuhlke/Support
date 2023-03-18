589addc30667d27c15a7be52fb0cf4f1c68322af
Mo Ramezanpoor <me@mohsenr.com>
2023-03-18 11:36:44 +0000

    Ensure `URLSessionHTTPClient` uses the session provided to it.

---
+++ Tests/SupportTests/Networking/URLSessionHTTPClientTests.swift
@@ -0,0 +1,33 @@
+import Foundation
+import Support
+import TestingSupport
+import XCTest
+
+class URLSessionHTTPClientTests: XCTestCase {
+    
+    func testClientUsesTheInjectedURLSession() async throws {
+        let remote = HTTPRemote(host: "example.com", path: "")
+        let data = Data.random()
+        let session = MockURLSession(
+            data: data,
+            response: HTTPURLResponse(url: try! remote.url(for: .get(""), scheme: .https), statusCode: 200, httpVersion: nil, headerFields: nil)!
+        )
+        let client = URLSessionHTTPClient(remote: remote, session: session)
+        
+        let request = HTTPRequest.get("")
+        let response = try await client.perform(request).get()
+        TS.assert(response.body.content, equals: data)
+    }
+    
+}
+
+private struct MockURLSession: URLSessionProtocol {
+    
+    var data: Data
+    var response: HTTPURLResponse
+    
+    func data(for request: URLRequest, delegate: URLSessionTaskDelegate?) async throws -> (Data, URLResponse) {
+        return (data, response)
+    }
+    
+}
