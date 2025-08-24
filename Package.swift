0e4e33d1c2c522fd03f845e3559d0531f1dfd1a2
Mo Ramezanpoor <me@mohsenr.com>
2025-08-24 14:28:03 +0100

    Update minimum supported OS versions.

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "Support",
     platforms: [
-        .macOS(.v12),
-        .iOS(.v15),
-        .tvOS(.v15),
-        .watchOS(.v8),
+        .macOS(.v14),
+        .iOS(.v17),
+        .tvOS(.v17),
+        .watchOS(.v10),
     ],
     products: [
         .library(
