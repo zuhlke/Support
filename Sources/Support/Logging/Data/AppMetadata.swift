e55cfe54006ab0bf0d5fb8d250a93ad20203b481
Mo Ramezanpoor <me@mohsenr.com>
2025-09-20 12:42:35 +0100

    Provide API to load AppMetadata from bundles.

--- Sources/Support/Logging/Data/AppMetadata.swift
+++ Sources/Support/Logging/Data/AppMetadata.swift
@@ -18,3 +18,27 @@ public struct AppMetadata: Sendable {
         self.displayName = displayName
     }
 }
+
+extension AppMetadata {
+    
+    /// Loads AppMetadata from a bundle.
+    ///
+    /// `bundle` must have an identifier, and at least one of bundle name or display name set. Otherwise `init` will return `nil`.
+    public init?(from bundle: Bundle) {
+        guard
+            let bundleIdentifier = bundle.bundleIdentifier,
+            let infoDictionary = bundle.infoDictionary,
+            let displayName = (infoDictionary["CFBundleDisplayName"] as? String) ?? (infoDictionary["CFBundleName"] as? String) else {
+            return nil
+        }
+        
+        self.init(
+            bundleIdentifier: bundleIdentifier,
+            displayName: displayName
+        )
+    }
+    
+    /// AppMetadata associated with the main bundle.
+    public static let main = AppMetadata(from: .main)!
+    
+}
---
+++ Tests/SupportTests/Logging/Data/AppMetadataTests.swift
@@ -0,0 +1,78 @@
+
+import Foundation
+import Testing
+import Support
+import TestingSupport
+
+@Suite
+struct AppMetadataTests {
+    @Test func `Can’t make metadata from empty bundle`() async throws {
+        try Bundle.withTemporaryBundle { bundle in
+            #expect(AppMetadata(from: bundle) == nil)
+        }
+    }
+    
+    @Test func `Make metadata from bundle with display name and identifier`() async throws {
+        let bundleIdentifier = String.random()
+        let displayName = String.random()
+        let info = [
+            "CFBundleIdentifier": bundleIdentifier,
+            "CFBundleDisplayName": displayName,
+        ]
+        try Bundle.withTemporaryBundle(info: info) { bundle in
+            let metadata = try #require(AppMetadata(from: bundle))
+            #expect(metadata.bundleIdentifier == bundleIdentifier)
+            #expect(metadata.displayName == displayName)
+        }
+    }
+    
+    @Test func `Make metadata from bundle with bundle name and identifier`() async throws {
+        let bundleIdentifier = String.random()
+        let bundleName = String.random()
+        let info = [
+            "CFBundleIdentifier": bundleIdentifier,
+            "CFBundleName": bundleName,
+        ]
+        try Bundle.withTemporaryBundle(info: info) { bundle in
+            let metadata = try #require(AppMetadata(from: bundle))
+            #expect(metadata.bundleIdentifier == bundleIdentifier)
+            #expect(metadata.displayName == bundleName)
+        }
+    }
+    
+    @Test func `Make metadata uses display name over bundle name`() async throws {
+        let bundleIdentifier = String.random()
+        let displayName = String.random()
+        let info = [
+            "CFBundleIdentifier": bundleIdentifier,
+            "CFBundleDisplayName": displayName,
+            "CFBundleName": .random(),
+        ]
+        try Bundle.withTemporaryBundle(info: info) { bundle in
+            let metadata = try #require(AppMetadata(from: bundle))
+            #expect(metadata.bundleIdentifier == bundleIdentifier)
+            #expect(metadata.displayName == displayName)
+        }
+    }
+    
+    @Test func `Make metadata from main bundle`() async throws {
+        let metadata = AppMetadata.main
+        #expect(metadata.bundleIdentifier == Bundle.main.bundleIdentifier!)
+        #expect(metadata.displayName == Bundle.main.infoDictionary!["CFBundleName"] as! String)
+    }
+}
+
+private extension Bundle {
+    
+    static func withTemporaryBundle<T>(info: [String: String] = [:], work closure: (Bundle) throws -> T) throws -> T {
+        let fileManager = FileManager()
+        return try fileManager.withTemporaryDirectory { url in
+            let infoPlistURL = url.appending(component: "Info.plist")
+            try PropertyListEncoder().encode(info).write(to: infoPlistURL)
+            
+            let bundle = try #require(Bundle(url: url))
+            return try closure(bundle)
+        }
+    }
+    
+}
