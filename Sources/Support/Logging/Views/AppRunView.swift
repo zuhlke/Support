4c9f794195dcd9a0e9041738342a4bcf7d2ea21b
Ryan Collins <ryan.collins@zuhlke.com>
2025-09-22 22:52:28 +0100

    Add previews and tokens

--- Sources/Support/Logging/Views/AppRunView.swift
+++ Sources/Support/Logging/Views/AppRunView.swift
@@ -3,6 +3,41 @@
 import SwiftUI
 import SwiftData
 
+enum LogToken: Identifiable, Hashable {
+    case level(String)
+    case subsystem(String)
+    case category(String)
+    
+    var id: String {
+        switch self {
+        case .level(let value):
+            return value
+        case .category(let value):
+            return value
+        case .subsystem(let value):
+            return value
+        }
+    }
+    
+    var displayName: String {
+        switch self {
+        case .level(let value):
+            return "Level: \(value)"
+        case .category(let value):
+            return "Category: \(value)"
+        case .subsystem(let value):
+            return "Subsystem: \(value)"
+        }
+    }
+}
+
+struct Token: Identifiable {
+    var name: String
+    var id: String {
+        name
+    }
+}
+
 @available(iOS 26.0, *)
 @available(macOS, unavailable)
 struct AppRunView: View {
@@ -12,6 +47,31 @@ struct AppRunView: View {
     let items = ["Level", "Date", "Subsystem", "Category"]
     @State var selection = Set<String>(["Level", "Date", "Subsystem", "Category"])
     @State private var searchText = ""
+    @State private var logTokens: [LogToken] = [.level("Notice")]
+    
+    var levels: [String] {
+        logEntries.compactMap { logEntry in
+            logEntry.level?.exportDescription
+        }
+    }
+    
+    var subsystems: [String] {
+        logEntries.compactMap { logEntry in
+            logEntry.subsystem
+        }
+    }
+    
+    var categories: [String] {
+        logEntries.compactMap { logEntry in
+            logEntry.category
+        }
+    }
+    
+    var possibleTokens: [Token] {
+        (levels + subsystems + categories).map(Token.init)
+    }
+    
+    @State var currentTokens = [Token]()
     
     var filteredEntries: [LogEntry] {
         let trimmedSearchText = searchText.trimmingCharacters(in: .whitespaces)
@@ -46,7 +106,7 @@ struct AppRunView: View {
         ) { $0.appRun }
     }
     
-    var body: some View {
+    var logList: some View {
         List {
             ForEach(groupedEntries.keys.sorted { $0.launchDate > $1.launchDate }, id: \.self) { appRun in
                 Section(appRun.launchDate.formatted()) {
@@ -73,7 +133,21 @@ struct AppRunView: View {
                 }
             }
         }
-        .searchable(text: $searchText)
+    }
+    
+    var body: some View {
+        logList
+            .searchable(text: $searchText, tokens: $currentTokens) { token in
+            Text(token.name)
+        }
+        .onChange(of: searchText) {
+            if let token = searchText.endWithToken(possibleTokens) {
+                if !currentTokens.contains(where: { $0.name == token.name }) {
+                    searchText.removeLast(token.name.count)
+                    currentTokens.append(token)
+                }
+            }
+        }
         .toolbar {
             ToolbarSpacer(.flexible, placement: .bottomBar)
             if isFilterMenuShown {
@@ -108,6 +182,56 @@ struct AppRunView: View {
         }
         .animation(.easeInOut, value: isFilterMenuShown)
     }
+}
+
+
+extension String {
+    func endWithToken(_ types: [Token]) -> Token? {
+        let selfLowercased = self.lowercased()
+        for type in types {
+            if selfLowercased.hasSuffix(type.name.lowercased()) {
+                return type
+            }
+        }
+        return nil
+    }
+}
+
+struct SampleData: PreviewModifier {
+    static func makeSharedContext() throws -> ModelContainer {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: AppRun.self, configurations: config)
+        
+        let appRun = AppRun(appVersion: "1.0.0", operatingSystemVersion: "14.0.0", launchDate: Date(), device: "iPhone 12 Pro Max")
+        let logEntries = [
+            LogEntry(appRun: appRun, date: Date(), composedMessage: "Hello, world!", level: .info, category: "App", subsystem: "com.zuhlke.com"),
+            LogEntry(appRun: appRun, date: Date(), composedMessage: "Bye, world!", level: .info, category: "App", subsystem: "com.zuhlke.com")
+        ]
+        
+        let context = ModelContext(container)
+        context.insert(appRun)
+        context.insert(contentsOf: logEntries)
+        return container
+    }
+    
+    
+    func body(content: Content, context: ModelContainer) -> some View {
+        content.modelContainer(context)
+    }
+}
+
+extension PreviewTrait where T == Preview.ViewTraits {
+
+    @available(iOS 26.0, *)
+    @MainActor public static var sampleData: PreviewTrait<Preview.ViewTraits> {
+        return .modifier(SampleData())
+    }
+}
+
+
+@available(iOS 26.0, *)
+#Preview(traits: .sampleData) {
+    AppRunView()
 }
 
 #endif
