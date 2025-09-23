0e559e2a76877f86d9f9bfb3fce2f12af0d17aa7
Mo Ramezanpoor <me@mohsenr.com>
2025-09-23 11:18:21 +0100

    `AppGroupLogView` stores list of apps as a property.

--- Sources/Support/Logging/Views/AppGroupLogView.swift
+++ Sources/Support/Logging/Views/AppGroupLogView.swift
@@ -7,15 +7,19 @@ import SwiftData
 @available(iOS 26.0, *)
 @available(macOS, unavailable)
 public struct AppGroupLogView: View {
-    var convention: LogStorageConvention
+    
+    // TODO: P2 – Dynamically update list of apps as they are detected
+    // This first needs a change in `LogRetriever` to publish this data.
+    var apps: [AppLogContainer]
     
     public init(convention: LogStorageConvention) {
-        self.convention = convention
+        let logRetriever = try! LogRetriever(convention: convention)
+        apps = try! logRetriever.apps
+            .sorted(using: KeyPathComparator(\.bundleIdentifier))
     }
     
     var modalContainers: [String: ModelContainer] {
-        let logRetriever = try! LogRetriever(convention: convention)
-        let executables = try! logRetriever.apps.flatMap(\.executables)
+        let executables = try! apps.flatMap(\.executables)
         return Dictionary(uniqueKeysWithValues: executables.map { executable in
             let modalContainer = try! ModelContainer(for: AppRun.self, configurations: ModelConfiguration(url: executable.url))
             return (executable.bundleIdentifier, modalContainer)
