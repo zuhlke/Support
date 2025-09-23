57acfd56b53ca3a93f2f02d9401daa39a285cce5
Mo Ramezanpoor <me@mohsenr.com>
2025-09-23 11:06:34 +0100

    Rename `AppLogs` to `AppLogContainer` and document it.

--- Sources/Support/Logging/LogRetriever.swift
+++ Sources/Support/Logging/LogRetriever.swift
@@ -25,25 +25,21 @@ public class LogRetriever {
             .map { Executable(url: $0, bundleIdentifier: $0.deletingPathExtension().lastPathComponent) }
     }
     
-    public var apps: [AppLogs] {
+    public var apps: [AppLogContainer] {
         get throws {
             switch convention.executableTargetGroupingStrategy {
             case .none:
-                return try getExecutables(url: logsFolder).map { AppLogs(bundleIdentifier: $0.bundleIdentifier, executables: [$0]) }
+                return try getExecutables(url: logsFolder).map { AppLogContainer(bundleIdentifier: $0.bundleIdentifier, executables: [$0]) }
             case .byAppBundleIdentifier(let pathExtension):
                 let contents = try fileManager.contentsOfDirectory(at: logsFolder, includingPropertiesForKeys: nil)
                 return try contents
                     .filter { $0.pathExtension == pathExtension }
-                    .map { AppLogs(bundleIdentifier: $0.deletingPathExtension().lastPathComponent, executables: try getExecutables(url: $0)) }
+                    .map { AppLogContainer(bundleIdentifier: $0.deletingPathExtension().lastPathComponent, executables: try getExecutables(url: $0)) }
             }
         }
     }
 }
 
-public struct AppLogs {
-    public var bundleIdentifier: String
-    public var executables: [Executable]
-}
 
 public struct Executable: Hashable {
     public var url: URL
