32f78b48fa4e273c5ed80f4b51b79ab5745d32e8
Rahul Katariya <rahulkatariya@me.com>
2025-09-25 12:51:49 +0100

    Retrieve apps from AppManifest file in the LogRetriever

--- Sources/Support/Logging/LogRetriever.swift
+++ Sources/Support/Logging/LogRetriever.swift
@@ -13,22 +13,51 @@ public class LogRetriever {
         diagnosticsDirectory = try fileManager.url(for: convention.baseStorageLocation)
             .appending(components: convention.basePathComponents)
     }
-    
-    private func getExecutables(url: URL) throws -> [ExecutableLogContainer] {
-        let contents = try fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: nil)
-        return contents
-            .filter { $0.pathExtension == convention.logsFileExtension }
-            .map { ExecutableLogContainer(url: $0, id: $0.deletingPathExtension().lastPathComponent) }
+
+    private var executables: [ExecutableLogContainer] {
+        get throws {
+            let logsDirectory = diagnosticsDirectory.appending(component: convention.logsDirectory)
+            let contents = try fileManager.contentsOfDirectory(at: logsDirectory, includingPropertiesForKeys: nil)
+            return contents
+                .filter { $0.pathExtension == convention.logsFileExtension }
+                .map { ExecutableLogContainer(url: $0, id: $0.deletingPathExtension().lastPathComponent) }
+        }
     }
-    
+
+    private var manifests: [AppLogManifest] {
+        get throws {
+            let manifestDirectory = diagnosticsDirectory.appending(component: convention.manifestDirectory)
+            let contents = try fileManager.contentsOfDirectory(at: manifestDirectory, includingPropertiesForKeys: nil)
+            let decoder = JSONDecoder()
+            return try contents
+                .filter { $0.pathExtension == "json" } // TODO: (P3) - Use UTType
+                .map {
+                    try decoder.decode(AppLogManifest.self, from: Data(contentsOf: $0))
+                }
+        }
+    }
+
     public var apps: [AppLogContainer] {
         get throws {
-            return try getExecutables(url: diagnosticsDirectory.appending(component: convention.logsDirectory))
-                .map { AppLogContainer(id: $0.id, executables: [$0]) }
+            let manifests = try manifests
+            let executables = try executables
+            let executablesDictionary = Dictionary(uniqueKeysWithValues: executables.lazy.map {
+                ($0.id, $0)
+            })
+        
+            return manifests.map {
+                var executables = $0.extensions.compactMap {
+                    executablesDictionary[$0.key]
+                }
+                
+                if let appExectuable = executablesDictionary[$0.id] {
+                    executables.insert(appExectuable, at: 0)
+                }
+                
+                return AppLogContainer(id: $0.id, executables: executables)
+            }
         }
     }
 }
-
-
 
 #endif
