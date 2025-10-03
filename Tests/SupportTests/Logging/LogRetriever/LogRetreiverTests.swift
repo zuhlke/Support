7dc5f8213a67ef3f9454ba7e0adef09e3309ab02
Rahul Katariya <rahulkatariya@me.com>
2025-10-03 13:43:51 +0100

    Add tests for extensions

--- Tests/SupportTests/Logging/LogRetriever/LogRetreiverTests.swift
+++ Tests/SupportTests/Logging/LogRetriever/LogRetreiverTests.swift
@@ -71,6 +71,127 @@ struct LogRetreiverTests {
             )
         }
     }
+
+    @Test
+    func testInitWithValidConvention_withLogsForAppAndExtension() async throws {
+        try await FileManager().withTemporaryDirectory { url in
+            let fileManager = FileManager()
+            let convention = LogStorageConvention(
+                baseStorageLocation: .customLocation(url: url),
+                basePathComponents: ["Test"]
+            )
+            
+            // Create manifest file
+            let manifestsDir = url.appending(path: "Test/Manifests")
+            try fileManager.createDirectory(at: manifestsDir, withIntermediateDirectories: true)
+            
+            let manifest = AppLogManifest(
+                manifestVersion: 1,
+                id: "com.zuhlke.Support",
+                name: "Support",
+                extensions: [
+                    "com.zuhlke.Support.extension": .init(
+                        name: "SupportExtension",
+                        extensionPointIdentifier: "com.apple.widgetkit-extension"
+                    )
+                ]
+            )
+            let manifestData = try JSONEncoder().encode(manifest)
+            let manifestFile = manifestsDir.appending(path: "com.zuhlke.Support.json")
+            try manifestData.write(to: manifestFile)
+            
+            // Create log file
+            let logsDir = url.appending(path: "Test/Logs")
+            try fileManager.createDirectory(at: logsDir, withIntermediateDirectories: true)
+            let logFile = logsDir.appending(path: "com.zuhlke.Support.logs")
+            try "<none>".write(to: logFile, atomically: true, encoding: .utf8)
+            
+            let extensionLogFile = logsDir.appending(path: "com.zuhlke.Support.extension.logs")
+            try "<none>".write(to: extensionLogFile, atomically: true, encoding: .utf8)
+            
+            let retriever = try LogRetriever(convention: convention)
+            
+            let apps = try await retriever.appsStream.first { _ in true }
+            #expect(
+                apps == [
+                    AppLogContainer(
+                        id: "com.zuhlke.Support",
+                        displayName: "Support",
+                        executables: [
+                            ExecutableLogContainer(
+                                url: URL(string: "file:///private")!.appending(path: logFile.path()), // TODO: (P3) - Review the URL assertion.
+                                id: "com.zuhlke.Support",
+                                displayName: "Support",
+                                packageType: .mainApp
+                            ),
+                            ExecutableLogContainer(
+                                url: URL(string: "file:///private")!.appending(path: extensionLogFile.path()), // TODO: (P3) - Review the URL assertion.
+                                id: "com.zuhlke.Support.extension",
+                                displayName: "SupportExtension",
+                                packageType: .extension(extensionPointIdentifier: "com.apple.widgetkit-extension")
+                            )
+                        ]
+                    )
+                ]
+            )
+        }
+    }
+
+    @Test
+    func testInitWithValidConvention_withLogsForAppAndExtension_withoutLogFile() async throws {
+        try await FileManager().withTemporaryDirectory { url in
+            let fileManager = FileManager()
+            let convention = LogStorageConvention(
+                baseStorageLocation: .customLocation(url: url),
+                basePathComponents: ["Test"]
+            )
+            
+            // Create manifest file
+            let manifestsDir = url.appending(path: "Test/Manifests")
+            try fileManager.createDirectory(at: manifestsDir, withIntermediateDirectories: true)
+            
+            let manifest = AppLogManifest(
+                manifestVersion: 1,
+                id: "com.zuhlke.Support",
+                name: "Support",
+                extensions: [
+                    "com.zuhlke.Support.extension": .init(
+                        name: "SupportExtension",
+                        extensionPointIdentifier: "com.apple.widgetkit-extension"
+                    )
+                ]
+            )
+            let manifestData = try JSONEncoder().encode(manifest)
+            let manifestFile = manifestsDir.appending(path: "com.zuhlke.Support.json")
+            try manifestData.write(to: manifestFile)
+            
+            // Create log file
+            let logsDir = url.appending(path: "Test/Logs")
+            try fileManager.createDirectory(at: logsDir, withIntermediateDirectories: true)
+            let logFile = logsDir.appending(path: "com.zuhlke.Support.logs")
+            try "<none>".write(to: logFile, atomically: true, encoding: .utf8)
+
+            let retriever = try LogRetriever(convention: convention)
+            
+            let apps = try await retriever.appsStream.first { _ in true }
+            #expect(
+                apps == [
+                    AppLogContainer(
+                        id: "com.zuhlke.Support",
+                        displayName: "Support",
+                        executables: [
+                            ExecutableLogContainer(
+                                url: URL(string: "file:///private")!.appending(path: logFile.path()), // TODO: (P3) - Review the URL assertion.
+                                id: "com.zuhlke.Support",
+                                displayName: "Support",
+                                packageType: .mainApp
+                            )
+                        ]
+                    )
+                ]
+            )
+        }
+    }
 }
 
 #endif
