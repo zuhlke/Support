ecfcf998c650c1d19716fca9578a6335c978f739
Ryan Collins <ryan.collins@zuhlke.com>
2025-10-08 15:24:06 +0100

    Default logs to 2 lines only unless expanded

--- Sources/Support/Logging/Views/LogEntryView.swift
+++ Sources/Support/Logging/Views/LogEntryView.swift
@@ -9,6 +9,8 @@ struct LogEntryView: View {
     var tokens: [SearchToken]
     var isShowingMetadata: Set<Metadata>
     
+    @State private var isCollapsed: Bool = true
+    
     func scopeSection(text: String, for scope: SearchScope) -> some View {
         HStack(spacing: 2) {
             Image(systemName: scope.image)
@@ -20,36 +22,56 @@ struct LogEntryView: View {
     }
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            Text(entry.composedMessage.highlighted(
-                matching: [searchText] + tokens.filter { $0.scope == .message }.map { $0.text }
-            ))
-            HStack(spacing: 8) {
-                if let level = entry.level, isShowingMetadata.contains(.level) {
-                    LevelView(level)
+        Button {
+            isCollapsed.toggle()
+        } label: {
+            VStack(alignment: .leading, spacing: 4) {
+                Text(entry.composedMessage.highlighted(
+                    matching: [searchText] + tokens.filter { $0.scope == .message }.map { $0.text }
+                ))
+                .lineLimit(isCollapsed ? 2 : nil)
+                HStack(spacing: 8) {
+                    if let level = entry.level, isShowingMetadata.contains(.level) {
+                        LevelView(level)
+                    }
+                    
+                    if isShowingMetadata.contains(.timestamp) {
+                        Text(entry.date.formatted(.dateTime
+                            .hour(.twoDigits(amPM: .omitted))
+                            .minute(.twoDigits)
+                            .second(.twoDigits)
+                            .secondFraction(.fractional(4))
+                        ))
+                    }
+                    
+                    if let subsystem = entry.subsystem, isShowingMetadata.contains(.subsystem) {
+                        scopeSection(text: subsystem, for: .subsystem)
+                    }
+                    
+                    if let category = entry.category, isShowingMetadata.contains(.category) {
+                        scopeSection(text: category, for: .category)
+                    }
                 }
-                
-                if isShowingMetadata.contains(.timestamp) {
-                    Text(entry.date.formatted(.dateTime
-                        .hour(.twoDigits(amPM: .omitted))
-                        .minute(.twoDigits)
-                        .second(.twoDigits)
-                        .secondFraction(.fractional(4))
-                    ))
-                }
-                
-                if let subsystem = entry.subsystem, isShowingMetadata.contains(.subsystem) {
-                    scopeSection(text: subsystem, for: .subsystem)
-                }
-                
-                if let category = entry.category, isShowingMetadata.contains(.category) {
-                    scopeSection(text: category, for: .category)
-                }
+                .font(.caption)
             }
-            .font(.caption)
         }
+        .buttonStyle(.plain)
+        .animation(.linear, value: isCollapsed)
     }
 }
+
+@available(iOS 26.0, *)
+#Preview {
+    let launchDate = Date()
+    
+    let appRun = AppRun(appVersion: "1.0.0", operatingSystemVersion: "14.0.0", launchDate: launchDate, device: "iPhone 12 Pro Max")
+    
+    let logEntry = LogEntry(appRun: appRun, date: launchDate.advanced(by: 1 * 60), composedMessage: "This is a really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really long message", level: .info, category: "App", subsystem: "com.zuhlke.test")
+    
+    LogEntryView(entry: logEntry, searchText: "", tokens: [], isShowingMetadata: .init(Metadata.allCases))
+        .padding(16)
+}
+
 
 #endif
 #endif
