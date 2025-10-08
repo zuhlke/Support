bdf66b5e18c202761fb18334554ba535dab2596b
Rahul Katariya <rahulkatariya@me.com>
2025-10-08 15:52:26 +0100

    Add test for logs after initializing log monitor

--- Tests/SupportTests/Logging/LogMonitor/LogMonitorTests.swift
+++ Tests/SupportTests/Logging/LogMonitor/LogMonitorTests.swift
@@ -12,7 +12,7 @@ struct LogMonitorTests {
         try fileManager.withTemporaryDirectory { url in
             let logStore = LogStore(entries: [])
             
-            let _ = try LogMonitor(
+            let logMonitor = try LogMonitor(
                 convention: LogStorageConvention(
                     baseStorageLocation: .customLocation(url: url),
                     basePathComponents: ["Test"]
@@ -55,7 +55,7 @@ struct LogMonitorTests {
         try fileManager.withTemporaryDirectory { url in
             let logStore = LogStore(entries: [])
             
-            let _ = try LogMonitor(
+            let logMonitor = try LogMonitor(
                 convention: LogStorageConvention(
                     baseStorageLocation: .customLocation(url: url),
                     basePathComponents: ["Test"]
@@ -129,7 +129,64 @@ struct LogMonitorTests {
                 LogEntry(composedMessage: "Log message", date: .init(timeIntervalSince1970: 1))
             ])
             
-            let _ = try LogMonitor(
+            let logMonitor = try LogMonitor(
+                convention: LogStorageConvention(
+                    baseStorageLocation: .customLocation(url: url),
+                    basePathComponents: ["Test"]
+                ),
+                bundleMetadata: BundleMetadata(
+                    id: "com.zuhlke.Support",
+                    name: "Support",
+                    version: "1",
+                    shortVersionString: "1",
+                    packageType: .app(.init(plugins: []))
+                ),
+                deviceMetadata: DeviceMetadata(
+                    operatingSystemVersion: "26.0",
+                    deviceModel: "iPhone 17 Pro"
+                ),
+                logStore: logStore,
+                appLaunchDate: .init(timeIntervalSince1970: 1)
+            )
+            
+            let logFile = url.appending(path: "Test/Logs/com.zuhlke.Support.logs")
+            let configuration = ModelConfiguration(url: logFile, cloudKitDatabase: .none)
+            let modelContainer = try ModelContainer(
+                for: AppRun.self,
+                configurations: configuration
+            )
+            let context = ModelContext(modelContainer)
+            let descriptor = FetchDescriptor<AppRun>(predicate: nil, sortBy: [SortDescriptor(\.launchDate)])
+            
+            // FIXME: - Remove sleep and listen when the change occurred.
+            try await Task.sleep(for: .seconds(1))
+            let runs = try context.fetch(descriptor)
+            
+            // FIXME: - Assert AppRun instead of Snapshots
+            let appRunSnapshots = runs.map(\.snapshot)
+            #expect(appRunSnapshots == [AppRun.Snapshot(
+                info: .init(
+                    appVersion: "1",
+                    operatingSystemVersion: "26.0",
+                    launchDate: .init(timeIntervalSince1970: 1),
+                    device: "iPhone 17 Pro"
+                ),
+                logEntries: [
+                    .init(date: .init(timeIntervalSince1970: 1), composedMessage: "Log message")
+                ]
+            )])
+        }
+    }
+
+    @Test
+    func fetchingLogs_afterInit_storesLogEntriesInDatabase() async throws {
+        let fileManager = FileManager()
+        try await fileManager.withTemporaryDirectory { url in
+            let logStore = LogStore(entries: [
+                LogEntry(composedMessage: "Log message", date: .init(timeIntervalSince1970: 2))
+            ])
+            
+            let logMonitor = try LogMonitor(
                 convention: LogStorageConvention(
                     baseStorageLocation: .customLocation(url: url),
                     basePathComponents: ["Test"]
@@ -158,23 +215,50 @@ struct LogMonitorTests {
             let context = ModelContext(modelContainer)
             let descriptor = FetchDescriptor<AppRun>(predicate: nil, sortBy: [SortDescriptor(\.launchDate)])
 
-            // FIXME: - Remove me.
-            try await Task.sleep(for: .seconds(1))
-            let runs = try context.fetch(descriptor)
-            
-            // FIXME: - Assert AppRun instead of Snapshots
-            let appRunSnapshots = runs.map(\.snapshot)
-            #expect(appRunSnapshots == [AppRun.Snapshot(
-                info: .init(
-                    appVersion: "1",
-                    operatingSystemVersion: "26.0",
-                    launchDate: .init(timeIntervalSince1970: 1),
-                    device: "iPhone 17 Pro"
-                ),
-                logEntries: [
-                    .init(date: .init(timeIntervalSince1970: 1), composedMessage: "Log message")
-                ]
-            )])
+            do {
+                // FIXME: - Remove sleep and listen when the change occurred.
+                try await Task.sleep(for: .seconds(2))
+                let runs = try context.fetch(descriptor)
+                
+                // FIXME: - Assert AppRun instead of Snapshots
+                let appRunSnapshots = runs.map(\.snapshot)
+                #expect(appRunSnapshots == [AppRun.Snapshot(
+                    info: .init(
+                        appVersion: "1",
+                        operatingSystemVersion: "26.0",
+                        launchDate: .init(timeIntervalSince1970: 1),
+                        device: "iPhone 17 Pro"
+                    ),
+                    logEntries: [
+                        .init(date: .init(timeIntervalSince1970: 2), composedMessage: "Log message")
+                    ]
+                )])
+            }
+
+            do {
+                logStore.log(entry: LogEntry(composedMessage: "Log message 2", date: .init(timeIntervalSince1970: 3)))
+                logStore.log(entry: LogEntry(composedMessage: "Log message 3", date: .init(timeIntervalSince1970: 4)))
+                
+                // FIXME: - Remove sleep and listen when the change occurred.
+                try await Task.sleep(for: .seconds(5))
+                let runs = try context.fetch(descriptor)
+                
+                // FIXME: - Assert AppRun instead of Snapshots
+                let appRunSnapshots = runs.map(\.snapshot)
+                #expect(appRunSnapshots == [AppRun.Snapshot(
+                    info: .init(
+                        appVersion: "1",
+                        operatingSystemVersion: "26.0",
+                        launchDate: .init(timeIntervalSince1970: 1),
+                        device: "iPhone 17 Pro"
+                    ),
+                    logEntries: [
+                        .init(date: .init(timeIntervalSince1970: 2), composedMessage: "Log message"),
+                        .init(date: .init(timeIntervalSince1970: 3), composedMessage: "Log message 2"),
+                        .init(date: .init(timeIntervalSince1970: 4), composedMessage: "Log message 3"),
+                    ]
+                )])
+            }
         }
     }
 }
