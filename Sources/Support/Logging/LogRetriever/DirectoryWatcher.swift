f2ca5f4d39c5c9edb8bb4ea5f6037520e28f8c60
Rahul Katariya <rahulkatariya@me.com>
2025-10-14 10:33:02 +0100

    Revert DirectoryWatcher

---
+++ Sources/Support/Logging/LogRetriever/DirectoryWatcher.swift
@@ -0,0 +1,88 @@
+#if canImport(Darwin)
+
+import Foundation
+import Dispatch
+
+class MultiDirectoryWatcher {
+    private var watchers: [DirectoryWatcher] = []
+    private let onDirectoryChange: (() -> Void)?
+
+    init(urls: [URL], onDirectoryChange: (() -> Void)? = nil) {
+        self.onDirectoryChange = onDirectoryChange
+        watchers = urls.map { url in
+            DirectoryWatcher(directoryURL: url) { [weak self] in
+                self?.onDirectoryChange?()
+            }
+        }
+    }
+
+    func startWatching() throws {
+        for watcher in watchers {
+            try watcher.startWatching()
+        }
+    }
+
+    func stopWatching() {
+        for watcher in watchers {
+            watcher.stopWatching()
+        }
+    }
+
+    deinit {
+        stopWatching()
+    }
+}
+
+enum DirectoryWatcherError: Error {
+    case failedToOpenDirectory(URL)
+}
+
+class DirectoryWatcher {
+    private let queue = DispatchQueue(label: "com.zuhlke.Support.DirectoryWatcher", qos: .utility)
+
+    private let directoryURL: URL
+    private var source: DispatchSourceFileSystemObject?
+    private let onDirectoryChange: (() -> Void)?
+
+    init(directoryURL: URL, onDirectoryChange: (() -> Void)? = nil) {
+        self.directoryURL = directoryURL
+        self.onDirectoryChange = onDirectoryChange
+    }
+
+    func startWatching() throws {
+        guard source == nil else { return }
+
+        let descriptor = open(directoryURL.path, O_EVTONLY)
+        guard descriptor >= 0 else {
+            throw DirectoryWatcherError.failedToOpenDirectory(directoryURL)
+        }
+
+        source = DispatchSource.makeFileSystemObjectSource(
+            fileDescriptor: descriptor,
+            eventMask: [.write],
+            queue: queue
+        )
+
+        source?.setEventHandler { [weak self] in
+            guard let self = self else { return }
+            self.onDirectoryChange?()
+        }
+
+        source?.setCancelHandler {
+            close(descriptor)
+        }
+
+        source?.resume()
+    }
+
+    func stopWatching() {
+        source?.cancel()
+        source = nil
+    }
+
+    deinit {
+        stopWatching()
+    }
+}
+
+#endif
